43cd99b0740788d056714119738645bfc10aeba9
sergei <leon44173@gmail.com>
2019-09-25 18:33:57 +0300

    внесены изменения

--- js/main.js
+++ js/main.js
@@ -31,3 +31,16 @@ $('.close').click(function () {
   modalParent.removeClass('show');
   $('body').removeClass('no-scroll');
 });
+/*********************  плавный скролл    ***************/
+$(document).ready(function(){
+  $('a[href^="#"]').click(function(){
+  elementClick = $(this).attr("href");
+  destination = $(elementClick).offset().top;
+  if($.browser){
+  $('body').animate( { scrollTop: destination }, 1000 );
+  } else {
+  $('html').animate( { scrollTop: destination }, 1000 );
+  }
+  return false;
+  });
+  });

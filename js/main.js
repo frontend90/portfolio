751b848e452badf9131a16758efb7fe9736df20e
frontend90 <leon44173@gmail.com>
2020-02-01 12:03:31 +0200

    внесены изменения

--- js/main.js
+++ js/main.js
@@ -32,15 +32,24 @@ $('.close').click(function () {
   $('body').removeClass('no-scroll');
 });
 /*********************  плавный скролл    ***************/
-$(document).ready(function(){
-  $('a[href^="#"]').click(function(){
-  elementClick = $(this).attr("href");
-  destination = $(elementClick).offset().top;
-  if($.browser){
-  $('body').animate( { scrollTop: destination }, 1000 );
-  } else {
-  $('html').animate( { scrollTop: destination }, 1000 );
-  }
-  return false;
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
   });
-  });
+});
+
+/**************    load more (btn)   ***********/
+$('.portfolio-cards button').click(function () {
+  $('.load-more').toggleClass('active');
+});
